85f4e6a311c6ebd1c7e149e94b873e214b4cd0a3
100182426 <JadynSlayer21>
2022-09-13 08:50:07 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,32 @@
+public class Variables
+{
+    public static void main ( String[] args )
+    {
+        //define 1 variable of each of the
+        //following data types
+        //byte	short		int 		long
+        //float	double
+        //char      boolean	String
+
+        //integer variables
+        byte byteOne = 127;
+        short a = 1;
+        int num1 = a;
+        long lnum = 1000;
+        float f1 = (float) 5.8;
+        double dnum = 105.6982;
+        char ch = 'A';
+        boolean school = true;
+        String schoolName = "Lubbock High School";
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person            07/18/22*");
+        System.out.println("*                               *");
+        System.out.println("*        integer types          *");
+        System.out.println("*                               *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+        System.out.println(num1 + f1);
+        System.out.println(ch + a);
+        System.out.println()
+    }
+}
